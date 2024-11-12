91b74f31c97a9d0afd08fa97204d0402fe34c2df
Royal Monteiro <royalmonteiro7@gmail.com>
2024-11-12 15:05:38 +0530

    12

---
+++ src/app/safe-link.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, ElementRef, inject, input } from '@angular/core';
+import { LogDirective } from './log.directive';
+
+@Directive({
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
+  hostDirectives: [LogDirective],
+})
+export class SafeLinkDirective {
+  queryParam = input('myapp', { alias: 'appSafeLink' });
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
+  constructor() {
+    console.log('SafeLinkDirective is active!');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
+
+    if (wantsToLeave) {
+      // const address = (event.target as HTMLAnchorElement).href;
+      // (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href =
+        address + '?from=' + this.queryParam();
+      return;
+    }
+
+    event.preventDefault();
+  }
+}
